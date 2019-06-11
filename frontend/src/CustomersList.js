4d16cd9895482f34557bb52eeb341ccbf237660d
Daniel Corcoran <danielc.92@outlook.com>
2019-06-11 15:17:02 +1000

    Added render logic for CustomerList component

--- frontend/src/CustomersList.js
+++ frontend/src/CustomersList.js
@@ -17,6 +17,7 @@ export default class CustomersList extends Component {
         this.handleDelete = this.handleDelete.bind(this);
     }
 
+    // Sets the initial state by calling getCustomers() method 
     componentDidMount() {
         var self = this;
         customersService.getCustomers().then(response => {
@@ -24,10 +25,64 @@ export default class CustomersList extends Component {
         });
     }
 
+    // Can be called to delete a customer and adjust the state accordingly
+    handleDelete() {
+        var self = this;
+        customersService.deleteCustomer({pk : pk}).then(()=>{
+            var newArray = self.state.customers.filter(function(obj) {
+                return obj.pk !== pk;
+            });
+            self.setState({customers: newArray})
+        });
+    }
+
+
+    // Sets the state for the next page customer data, as well as the next Page url
+    nextPage() {
+        var self = this;
+        customersService.getCustomersByURL(this.state.nextPageURL).then(response => {
+            self.setState({customers: response.data, nextPageURL: response.nextLink})
+        });
+    }
+
+
     render() {
         return (
             <React.Fragment>
+                <section className="section">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Address</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.customers.map(customer=>{
+                      
+                                    <tr key={customer.pk}
+                                    >
+                                        <td>{customer.first_name}</td>
+                                        <td>{customer.last_name}</td>
+                                        <td>{customer.phone}</td>
+                                        <td>{customer.email}</td>
+                                        <td>{customer.address}</td>
+                                        <td>{customer.description}</td>
+                                        <td><button onClick={(e)=> this.handleDelete(e, customer.pk)} className="button is-link">Update</button>
+                                        <a href={'/customer/' + customer.pk} className="button is-danger">Delete</a></td>
+                                    </tr>
+                            })}
+                        </tbody>
 
+                    </table>
+                    <button className="button is-primary" onClick={this.nextPage}>Next</button>
+                </section>
             </React.Fragment>
         )
     }
