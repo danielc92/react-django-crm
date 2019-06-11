fb0424bc3c1f5fc8e1d2cb327b7a0fd651d1a08e
Daniel Corcoran <danielc.92@outlook.com>
2019-06-11 11:27:49 +1000

    Added customer detail view

--- customers/views.py
+++ customers/views.py
@@ -55,3 +55,31 @@ def customers_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def customers_detail(request, pk):
+    """
+    Customers can be viewed, deleted or modified given a customer id/pk
+    """
+
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CustomerSerializer(customer, 
+                                        data=request.data, 
+                                        context = {'request': request})
+                                        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
