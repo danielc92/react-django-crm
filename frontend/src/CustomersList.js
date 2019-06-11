57ee4bdf44a9fbe2b4b5ecf487051ad568c7ca8f
Daniel Corcoran <danielc.92@outlook.com>
2019-06-11 14:49:04 +1000

    Added constructor, didmount to CustomerList Component

--- frontend/src/CustomersList.js
+++ frontend/src/CustomersList.js
@@ -3,12 +3,32 @@ import CustomersService from './CustomersService';
 
 const customersService = new CustomersService();
 
+
 export default class CustomersList extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            customers = [],
+            nextPageURL = ''
+        }
+        this.nextPage = this.nextPage.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    componentDidMount() {
+        var self = this;
+        customersService.getCustomers().then(response => {
+            self.setState({customers: response.data, nextPageURL: response.nextLink})
+        });
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+
+            </React.Fragment>
         )
     }
 }
